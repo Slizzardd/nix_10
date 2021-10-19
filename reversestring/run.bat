46e625ef8b894101748943fd505f60935399ff61
Slizzard <artem.dubenko@nure.ua>
2021-10-19 19:30:23 +0300

    add non-working code homework 2

--- reversestring/run.bat
+++ reversestring/run.bat
@@ -1,5 +1,1 @@
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  \
-    -Dfile=D:\code\nix_10\reversestring\target\reversestring.jar \
-    -DgroupId=ua.com.alevel -DartifactId=ua.com.alevel.reversestring \
-    -Dversion=1.0-SNAPSHOT -Dpackaging=jar \
-    -DlocalRepositoryPath=${master_project}/local-maven-repo
+mvn install:install-file -Dfile= D:\code\nix_10\reversestring\target\reversestring.jar -DgroupId=ua.com.alevel -DartifactId=reversestring -Dversion=1.0-SNAPSHOT -Dpackaging=jar
