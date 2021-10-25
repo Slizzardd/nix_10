07cbf2725e26020ec97a5d4b9293a7be81d8ebc8
Slizzard <artem.dubenko@nure.ua>
2021-10-25 14:18:52 +0300

    add level2 ex1

---
+++ module_1/src/main/java/ua/com/alevel/level2/StringValidation.java
@@ -0,0 +1,44 @@
+package ua.com.alevel.level2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StringValidation {
+
+    private String enterTheString(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите вашу строку:");
+        return scanner.next();
+    }
+
+    private boolean isBalanced(String line){
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> brackets = new HashMap<>();
+        brackets.put(']', '[');
+        brackets.put('}', '{');
+        brackets.put(')', '(');
+        for(var c : line.toCharArray()){
+            if(brackets.containsValue(c)){
+                stack.push(c);
+            }else{
+                if (brackets.containsKey(c)) {
+                    if(stack.isEmpty() || !brackets.get(c).equals(stack.pop())){
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public void outputConsole() {
+        var line = enterTheString();
+        if(isBalanced(line)){
+            System.out.println("Ваша строка сбалансирована");
+        }else{
+            System.out.println("Ваша строка не сбалансирована:( ");
+        }
+    }
+}
