dcb3b3fb2cb3d9f88e362dffb50f37f621e57764
egorfuntusov <funtushan@gmail.com>
2021-12-12 18:37:41 +0200

    add review

--- hw_7_ionio/src/main/java/ua/com/alevel/Start_Hw_7.java
+++ hw_7_ionio/src/main/java/ua/com/alevel/Start_Hw_7.java
@@ -3,6 +3,7 @@ package ua.com.alevel;
 import ua.com.alevel.task1.controller.MainController;
 
 public class Start_Hw_7 {
+
     public static void main(String[] args) {
         MainController mainController = new MainController();
         mainController.run();
